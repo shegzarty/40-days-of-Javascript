a0699f990570831a45b1bdbb051410beb083b422
silht.ion <adekunleyshegun@gmail.com>
2026-04-28 23:36:28 +0100

    Day02 of 40 Days of Javascript

---
+++ day02/index.js
@@ -0,0 +1,11 @@
+let name = "Adekunle";
+let age = 56;
+let studentStatus = "Graduated";
+let favoriteProgrammingLang = "Javascript";
+
+age = 80;
+studentStatus = "Not sure";
+console.log(name, age, studentStatus, favoriteProgrammingLang);
+
+
+const address 
